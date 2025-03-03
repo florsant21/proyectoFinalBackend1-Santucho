b2554ef5a56d087d15a856999ccdeb0c6b0589e6
Florencia Santucho <andreaf.santucho@gmail.com>
2025-03-03 01:00:07 -0300

    Soluciono errores para visualizar handlebars.

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ import express from "express";
 import connectDB from "./src/config/db.js";
 import productRoutes from "./src/routes/productRoutes.js";
 import cartRoutes from "./src/routes/cartRoutes.js";
-import Product from "./src/models/Product.js";
 import path from "path";
 import { fileURLToPath } from "url";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import dotenv from "dotenv";
+import exphbs from "express-handlebars";
+import Product from "./src/models/Product.js";
 
 dotenv.config();
 
@@ -21,10 +22,55 @@ const io = new Server(server);
 
 connectDB();
 
+const hbs = exphbs.create({
+  allowProtoPropertiesByDefault: true,
+  allowProtoMethodsByDefault: true,
+});
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.set("views", path.join(__dirname, "src", "views"));
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
+
 app.use("/api/products", productRoutes);
 app.use("/api/carts", cartRoutes);
+
+app.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.render("home", { products });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    const simpleProducts = products.map(product => ({
+      name: product.name,
+      price: product.price
+    }));
+    res.render("home", { products: simpleProducts });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/real-time-products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    const simpleProducts = products.map(product => ({
+      name: product.name,
+      price: product.price
+    }));
+    res.render("realTimeProducts", { products: simpleProducts });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 io.on("connection", (socket) => {
   console.log("Cliente conectado");
